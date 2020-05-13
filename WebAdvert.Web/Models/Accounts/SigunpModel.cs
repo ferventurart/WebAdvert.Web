822ea5cbb9be0ced862aa73e5fb86b349a9cb77b
Fernando Ventura <ferventurart@gmail.com>
2020-05-12 20:00:07 -0600

    First Commit

---
+++ WebAdvert.Web/Models/Accounts/SigunpModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAdvert.Web.Models.Accounts
+{
+    public class SigunpModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(6, ErrorMessage = "Password must be at least six characters long!")]
+        public string Password { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Password and its confirmation do not match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
